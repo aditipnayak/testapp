e55bbe501d03200cf2d06d47913d1e4fe5c291fc
aditipnayak <68282524+aditipnayak@users.noreply.github.com>
2022-10-21 00:23:38 +0530

    Delete TestFile.java

--- TestFile.java
+++
@@ -1,8 +0,0 @@
-public class TestFile {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int result = a + b;
-        System.out.println(result);
-    }
-}
