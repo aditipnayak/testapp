9168bfd275c0910e23857cedf296517993df9ea7
aditipnayak <68282524+aditipnayak@users.noreply.github.com>
2022-10-21 00:23:29 +0530

    Delete NewFile.java

--- NewFile.java
+++
@@ -1,8 +0,0 @@
-public class NewFile {
-    public static void main(String[] args) {
-        System.out.println("NewFile");
-        System.out.println("Welcome to git");
-        System.out.println("NewFile2");
-        System.out.println("Welcome to vs code");
-    }
-}
