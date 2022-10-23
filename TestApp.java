568c776b1d344932432ae4859fe690bc98675b63
aditipnayak <68282524+aditipnayak@users.noreply.github.com>
2022-10-23 20:11:25 +0530

    Update TestApp.java

--- TestApp.java
+++ TestApp.java
@@ -2,6 +2,5 @@ public class TestApp {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("Welcome");
-        System.out.println("Test");
     }
 }
