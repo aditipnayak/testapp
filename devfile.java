ed487725f36b3bc50bfd3f1558c03c4526c6e4fe
aditi <aditipnayak12@gmail.com>
2022-10-23 20:08:36 +0530

    dev commit

---
+++ devfile.java
@@ -0,0 +1,7 @@
+public class devfile {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Welcome");
+        System.out.println("Test");
+    }
+}
