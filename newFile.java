040981386ce95552ced0a625c5a473527a00ac19
aditipnayak <68282524+aditipnayak@users.noreply.github.com>
2022-10-23 20:11:05 +0530

    Delete newFile.java

--- newFile.java
+++
@@ -1,7 +0,0 @@
-public class newFile {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("Welcome");
-        System.out.println("Test");
-    }
-}
